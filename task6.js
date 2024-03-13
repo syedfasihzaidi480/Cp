7158dc2d49cbc777a1f98da85b3709110bdfd927
SYED MUHAMMAD FASIH UL HASSAN ZAIDI <159549280+syedfasihzaidi480@users.noreply.github.com>
2024-03-13 13:52:56 +0500

    TASK 6

---
+++ task6.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var totalWidth = $('#myText').outerWidth(true);
+    alert("Total width of the paragraph (including padding and border): " + totalWidth + "px");
+});
