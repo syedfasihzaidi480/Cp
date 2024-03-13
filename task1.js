ca68b817771755f783ae6ae4028b23d0cb4ffe91
SYED MUHAMMAD FASIH UL HASSAN ZAIDI <159549280+syedfasihzaidi480@users.noreply.github.com>
2024-03-13 13:49:10 +0500

    TASK 1

---
+++ task1.js
@@ -0,0 +1,8 @@
+function calculateArea(length, width) {
+    var area = length * width;
+    return area;
+}
+var length = 5;
+var width = 3;
+var area = calculateArea(length, width);
+console.log("The area of the rectangle is: " + area);
